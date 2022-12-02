425b8a089512284dd2a47ffb7ced8f6908ef74a4
Dario <me@dgrp.es>
2022-12-02 12:14:00 +0100

    Second part

--- src/day_1/main.rs
+++ src/day_1/main.rs
@@ -1,18 +1,52 @@
+use std::env;
 use std::io::{stdin, BufRead};
 
 fn main() {
+    if let Some(arg) = env::args().nth(1) {
+        if arg == "-1" {
+            first_part();
+        } else {
+            second_part();
+        }
+    } else {
+        println!("Specify the part of the test, -1 or -2");
+        println!("{} (-1 | -2)", env::args().nth(0).unwrap());
+    }
+}
+
+fn first_part() {
     let mut max_calories = 0;
-    let mut current = Vec::new();
+    let mut current = 0;
 
     for line in stdin().lines().map(|x| x.unwrap()) {
         if line.is_empty() {
-            let calories = current.iter().sum::<u64>();
-            current.clear();
-            max_calories = max_calories.max(calories);
+            current = 0;
         } else {
-            current.push(line.parse::<u64>().unwrap());
+            current += line.parse::<u64>().unwrap();
+            max_calories = max_calories.max(current);
         }
     }
 
     println!("{}", max_calories);
+}
+
+fn second_part() {
+    let mut results = Vec::new();
+    let mut current = 0;
+
+    for line in stdin().lines().map(|x| x.unwrap()) {
+        if line.is_empty() {
+            results.push(current);
+            current = 0;
+        } else {
+            current += line.parse::<u64>().unwrap();
+        }
+    }
+
+    results.push(current);
+
+    results.sort_by(|a, b| b.cmp(a));
+
+    let calories = results.iter().take(3).sum::<u64>();
+    println!("{}", calories);
 }
