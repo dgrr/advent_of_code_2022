2bb3298c2e6f9347b3ed7de31624e0d6d7fac338
Dario <me@dgrp.es>
2022-12-04 12:12:04 +0100

    day4

---
+++ src/day4.rs
@@ -0,0 +1,105 @@
+use std::env;
+use std::io::stdin;
+use std::time::Instant;
+
+fn main() {
+    if let Some(arg) = env::args().nth(1) {
+        if arg == "-1" {
+            first_part();
+        } else {
+            second_part();
+        }
+    } else {
+        println!("Specify the part of the test, -1 or -2");
+        println!("{} (-1 | -2)", env::args().nth(0).unwrap());
+    }
+}
+
+fn first_part() {
+    let started = Instant::now();
+    let res = stdin().lines()
+        .map(|x| x.unwrap())
+        .map(|x| {
+            let (first, second) = x.split_once(',').unwrap();
+            let first_range = first.split_once('-').unwrap();
+            let second_range = second.split_once('-').unwrap();
+
+            let range_a = (
+                first_range.0.parse::<u64>().unwrap(),
+                first_range.1.parse::<u64>().unwrap(),
+            );
+            let range_b = (
+                second_range.0.parse::<u64>().unwrap(),
+                second_range.1.parse::<u64>().unwrap(),
+            );
+
+            (range_a.0 >= range_b.0 && range_a.1 <= range_b.1) ||
+                (range_b.0 >= range_a.0 && range_b.1 <= range_a.1)
+        })
+        .map(|x| if x { 1u64 } else { 0u64 })
+        .sum::<u64>();
+
+    println!("{} in {:?}", res, started.elapsed());
+}
+
+fn second_part() {
+    let res = stdin().lines()
+        .map(|x| x.unwrap())
+        .map(|x| {
+            let (first, second) = x.split_once(',').unwrap();
+            let first_range = first.split_once('-').unwrap();
+            let second_range = second.split_once('-').unwrap();
+
+            let range_a = (
+                first_range.0.parse::<u64>().unwrap(),
+                first_range.1.parse::<u64>().unwrap(),
+            );
+            let range_b = (
+                second_range.0.parse::<u64>().unwrap(),
+                second_range.1.parse::<u64>().unwrap(),
+            );
+
+            (range_a.0 >= range_b.0 && range_a.0 <= range_b.1) ||
+                (range_b.0 >= range_a.0 && range_b.0 <= range_a.1)
+        })
+        .map(|x| if x { 1u64 } else { 0u64 })
+        .sum::<u64>();
+
+    println!("{}", res);
+
+    // let mut ranges: Vec<(u64, u64)> = stdin().lines()
+    //     .map(|x| x.unwrap())
+    //     .map(|x| {
+    //         let (first, second) = x.split_once(',').unwrap();
+    //         let first_range = first.split_once('-').unwrap();
+    //         let second_range = second.split_once('-').unwrap();
+
+    //         let range_a = (
+    //             first_range.0.parse::<u64>().unwrap(),
+    //             first_range.1.parse::<u64>().unwrap(),
+    //         );
+    //         let range_b = (
+    //             second_range.0.parse::<u64>().unwrap(),
+    //             second_range.1.parse::<u64>().unwrap(),
+    //         );
+
+    //         (
+    //             range_a.0.min(range_b.0),
+    //             range_a.1.max(range_b.1)
+    //         )
+    //     })
+    //     .collect();
+
+    // let mut res = 0;
+    // for i in 0..ranges.len() {
+    //     for j in i+1..ranges.len() {
+    //         let range_a = ranges[i];
+    //         let range_b = ranges[j];
+    //         if (range_a.0 >= range_b.0 && range_a.1 <= range_b.1) ||
+    //             (range_b.0 >= range_a.0 && range_b.1 <= range_a.1) {
+    //             res += 1u64;
+    //             break;
+    //         }
+    //     }
+    // }
+}
